f7cd4cb650ba2a520cacf59d64e90a5ec788024c
cangeus <cangeus07@gmail.com>
2023-10-30 19:38:39 +0700

    first commit

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
 
-# Load data from the CSV file
-df = pd.read_csv('data.csv')  # Ganti 'data.csv' dengan nama file CSV yang sesuai
+# sidebar
+# title dari sidebar
+st.sidebar.title("Navigation")
+# membuat select radio
+menu = st.sidebar.radio('goto', ["Beranda", "Perbandingan"])
 
-# Set the title of the web app
-st.title('Grafik Indeks Pendidikan Berdasarkan Tahun')
+if menu == "Beranda":
+    # mengambil filke beranda.py
+    import beranda
+    # menampilkan tampilan beranda diambil dari function tampil
+    beranda.tampil()
 
-# Dropdown input for selecting a year
-selected_year = st.selectbox('Pilih Tahun:', df['tahun'].unique())
+elif menu == "Perbandingan":
+    # mengambil filke perbandingan.py
+    import perbandingan
+    # menampilkan tampilan perbandingan diambil dari function tampil
+    perbandingan.tampil()
 
-# Filter data based on the selected year
-filtered_df = df[df['tahun'] == selected_year]
 
-# Create a Plotly bar chart
-fig = px.bar(
-    filtered_df,
-    x='provinsi',
-    y='indeks_pendidikan',
-    labels={'indeks_pendidikan': 'Indeks Pendidikan'},
-    title=f'Grafik Indeks Pendidikan Tahun {selected_year}'
-)
-
-# Display the chart
-st.plotly_chart(fig)
